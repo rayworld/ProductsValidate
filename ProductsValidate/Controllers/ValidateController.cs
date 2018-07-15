bd0f84932fa2125f2a4504eefd3058c671eff4cb
rayworld <casp@sina.com>
2018-07-15 20:51:59 +0800

    主页改成了Validate，照样运行

---
+++ ProductsValidate/Controllers/ValidateController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductsValidate.Controllers
+{
+    public class ValidateController : Controller
+    {
+        // GET: Validate
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
